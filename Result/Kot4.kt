d0955d5f2565117f26def9f3f3536c7dfdedf33e
Tompson_KOR <rlaxoan9327@jj.ac.kr>
2022-05-12 22:08:34 +0900

    4일차
    입력한 정수에 100을 더하는 프로그램

---
+++ Result/Kot4.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(){
+    var scanner = Scanner(System.`in`)
+
+    print("Input A number : ")
+    var input = scanner.nextInt()
+    var result = input + 100
+    println("result is $result")
+}
