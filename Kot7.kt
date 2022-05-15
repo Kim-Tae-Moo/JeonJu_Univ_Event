b7423e4454127c1f91ddf860ee74f1e454415b0b
Tompson_KOR <rlaxoan9327@jj.ac.kr>
2022-05-15 11:13:55 +0900

    7일차
    6일차 업로드의 수정..
    8을 입력하면 에러가 같이 출력되는 문제를 바로잡았음

---
+++ Kot7.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    print("input 1~100 :")
+    var input = scan.nextInt()
+    if (input >100) println("It is bigger than 100!")
+    else if (input < 0) println("It is Smaller than 0!")
+    else if (input == 50) println("Your input is 50!")
+    else if (input <= 50) println("Your input is smaller than 50!")
+    else if (input >= 50) println("Your input is bigger than 50")
+    else println("ERROR")
+}
